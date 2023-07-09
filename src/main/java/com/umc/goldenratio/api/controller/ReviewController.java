c44a84afd8e42f794c5106cdac811282dfe154b5
leew0nseok <zzang50aa@gmail.com>
2023-07-09 23:24:24 +0900

    [FEAT] : Review등록 코드 작성

--- src/main/java/com/umc/goldenratio/api/controller/ReviewController.java
+++ src/main/java/com/umc/goldenratio/api/controller/ReviewController.java
@@ -1,12 +1,25 @@
 package com.umc.goldenratio.api.controller;
 
+import com.umc.goldenratio.api.domain.entity.Review;
+import com.umc.goldenratio.api.dto.request.WriteReviewDto;
+import com.umc.goldenratio.api.service.ReviewService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/goldenratio/review")
 public class ReviewController {
-    public void reviewWrite(){
+    private final ReviewService reviewService;
+
+    @PostMapping("/{boardId}")
+    public ResponseEntity<Review> writeReview(@PathVariable Long boardId,
+                                              @RequestBody WriteReviewDto writeReviewDto){
+        Review writedReview = reviewService.write(boardId, writeReviewDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(writedReview);
 
     }
 }
