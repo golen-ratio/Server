69be7f827d52f18bdb5667369e77f3bd9c6fcfed
leew0nseok <zzang50aa@gmail.com>
2023-07-10 22:14:40 +0900

    [FEAT] : 특정 게시판의 Review 조회 코드 작성

--- src/main/java/com/umc/goldenratio/api/controller/ReviewController.java
+++ src/main/java/com/umc/goldenratio/api/controller/ReviewController.java
@@ -2,24 +2,34 @@ package com.umc.goldenratio.api.controller;
 
 import com.umc.goldenratio.api.domain.entity.Review;
 import com.umc.goldenratio.api.dto.request.WriteReviewDto;
+import com.umc.goldenratio.api.dto.response.ReviewDto;
 import com.umc.goldenratio.api.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/goldenratio/review")
+@RequestMapping("/goldenratio")
 public class ReviewController {
     private final ReviewService reviewService;
 
-    @PostMapping("/{boardId}")
+    @PostMapping("/review/{boardId}")
     public ResponseEntity<Review> writeReview(@PathVariable Long boardId,
                                               @RequestBody WriteReviewDto writeReviewDto){
         Review writedReview = reviewService.write(boardId, writeReviewDto);
 
         return ResponseEntity.status(HttpStatus.OK).body(writedReview);
+
+    }
+
+    @GetMapping("/review/{boardId}")
+    public ResponseEntity<List<ReviewDto>> inquireReview(@PathVariable Long boardId){
+        List<ReviewDto> reviews = reviewService.inquire(boardId);
+        return ResponseEntity.status(HttpStatus.OK).body(reviews);
 
     }
 
