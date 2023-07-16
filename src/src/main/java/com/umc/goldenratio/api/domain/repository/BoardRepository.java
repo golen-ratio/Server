174470f9d7ff75532e060137918bc4a9cb37f954
0727hb <88015596+0727hb@users.noreply.github.com>
2023-07-16 20:10:52 +0900

    Delete src/src directory

--- src/src/main/java/com/umc/goldenratio/api/domain/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.umc.goldenratio.api.domain.repository;
-
-import com.umc.goldenratio.api.domain.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
