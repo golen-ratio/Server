a1c967ff3c0618c36d94c71c5c890e6fe368969d
0727hb <dlghkqls0902@naver.com>
2023-07-20 00:50:32 +0900

    update

--- Server-main/src/main/java/com/umc/goldenratio/api/dto/request/WriteReviewDto.java
+++ Server-main/src/main/java/com/umc/goldenratio/api/dto/request/WriteReviewDto.java
@@ -1,6 +1,9 @@
 package com.umc.goldenratio.api.dto.request;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
