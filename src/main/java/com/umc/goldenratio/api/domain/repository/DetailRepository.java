45e289563011810245e7cd1c5f1d708fdfc9a2fb
DAHLIACHOI <cyj000424@naver.com>
2023-07-31 01:28:12 +0900

    [FEAT] : 칵테일, 숙취해소 게시판 수정

--- src/main/java/com/umc/goldenratio/api/domain/repository/DetailRepository.java
+++ src/main/java/com/umc/goldenratio/api/domain/repository/DetailRepository.java
@@ -1,7 +1,9 @@
 package com.umc.goldenratio.api.domain.repository;
 
+import com.umc.goldenratio.api.domain.entity.Board;
 import com.umc.goldenratio.api.domain.entity.Detail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DetailRepository extends JpaRepository<Detail,Long> {
+    Detail findByBoard(Board board);
 }
